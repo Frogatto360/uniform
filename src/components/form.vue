bf80a03e26cf14d20220dabd3ea7744ffe30ab38
Eduardo <eduardo.fgm.neto@gmail.com>
2023-12-02 14:50:42 -0300

    first commit

---
+++ src/components/form.vue
@@ -0,0 +1,22 @@
+<template>
+ <form action="">
+    <label for=""> Nome completo </label>
+    <input type="text">
+
+    <label for=""> Endereço de Email </label>
+    <input type="text">
+
+    <label for=""> Idade </label>
+    <input type="text">
+
+    <label for=""> Uma breve descrição de si mesmo </label>
+    <input type="text">
+
+ </form>
+</template>
+
+<script>
+    export default {
+        name: 'FormularioAvaliativo'
+    }
+</script>
