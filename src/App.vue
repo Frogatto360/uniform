d69c9803a5f10c93fb83d92b4e26dda17b6fa6c8
Eduardo <eduardo.fgm.neto@gmail.com>
2023-12-03 01:04:29 -0300

    enviando arquivos de estilo

--- src/App.vue
+++ src/App.vue
@@ -16,5 +16,19 @@
 </script>
 
 <style>
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 
+  body{
+    height: 100vh;
+    width: 100%;
+    background-color: #ebebeb;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 </style>
--- src/components/form.vue
+++ src/components/form.vue
@@ -1,17 +1,26 @@
 <template>
  <form action="">
-    <label for=""> Nome completo </label>
-    <input type="text">
+    <div class="__form-div">
+        <label for=""> <strong> Nome completo </strong> </label>
+        <input type="text" required>
+    </div>
 
-    <label for=""> Endereço de Email </label>
-    <input type="text">
+    <div class="__form-div">
+        <label for=""> <strong> Endereço de Email </strong> </label>
+        <input type="email" required>
+    </div>
+    
+    <div class="__form-div">
+        <label for=""> <strong> Idade </strong> </label>
+        <input type="number" required>
+    </div>
 
-    <label for=""> Idade </label>
-    <input type="text">
+    <div class="__form-div">
+        <label for=""> <strong> Uma Breve Descrição de Si Mesmo </strong> </label>
+        <textarea name="" id="" cols="30" rows="10" required> </textarea>
+    </div>
 
-    <label for=""> Uma breve descrição de si mesmo </label>
-    <input type="text">
-
+    <button> Enviar </button>
  </form>
 </template>
 
@@ -20,3 +29,38 @@
         name: 'FormularioAvaliativo'
     }
 </script>
+
+<style>
+    form{
+        background-color: white;
+        border-radius: 2px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 500px;
+        height: 500px;
+    }
+
+    label{
+        width: 100%;
+    }
+
+    input{
+        width: 100%;
+        border: 1px solid #ccc;
+        border-radius: 2px;
+    }
+
+    .__form-div{
+        width: 80%;
+        display: flex;
+        flex-direction: column;
+        margin-top: 25px;
+    }
+
+    button{
+        margin-top: 50px;
+        width: 25%;
+    }
+</style>
